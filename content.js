d5d2b5bc78480473635fb2fc6cd1bcccc08cc565
Odiri Udele <odiri@troveapp.co>
2023-12-21 14:47:15 +0000

    modify content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-
+displayNotice();
 
 function onReady() {
     var currentUrl = window.location.href;
@@ -11,10 +11,48 @@ function onReady() {
     }, 5000);
 }
 
+function hideDisplayNotice() {
+    responseContainer = document.getElementById('extension-notice-response-container');
+    if (responseContainer) {
+        responseContainer.style.display = 'none';
+    }
+}
 
+function displayNotice(response) {
+    var responseContainer = document.createElement('div');
+    responseContainer.id = 'extension-notice-response-container';
+    document.body.appendChild(responseContainer);
+
+    var responseClass = 'response-notice-popup';
+    responseContainer.innerHTML = `
+            <div class="${responseClass}">
+                <span>Page is being scanned for phishing</span>
+                <button id="closeNoticeButton">X</button>
+            </div>`;
+
+    var closeButton = responseContainer.querySelector('#closeNoticeButton');
+  
+    if (closeButton) {
+        closeButton.style.border = 'none';
+        closeButton.style.backgroundColor = 'transparent';
+        closeButton.style.color = '#000';
+        closeButton.style.fontSize = '16px';
+        closeButton.style.cursor = 'pointer';
+
+        closeButton.addEventListener('click', function() {
+            responseContainer.style.display = 'none';
+        });
+    }
+
+    // Hide the container after 3 seconds
+    setTimeout(function() {
+        responseContainer.style.display = 'none';
+    }, 6000);
+}
 
 
 function display(response) {
+    hideDisplayNotice();
     var responseContainer = document.createElement('div');
     responseContainer.id = 'extension-response-container';
     document.body.appendChild(responseContainer);
